49ce94e582ec70a3a8094217a12d3726c712a5f5
Nic Wise <nicw@fastchicken.co.nz>
2015-11-19 08:45:23 +1300

    first

---
+++ TweetLightning/TweetLightning/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TweetLightning
+//
+//  Created by Nic Wise on 19/11/15.
+//  Copyright © 2015 Nic Wise. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
